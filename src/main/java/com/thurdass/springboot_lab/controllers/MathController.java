09b1bf92aefa74a7de46f910678feed6e5533e5f
ThurDas <va4591073@gmail.com>
2026-07-13 22:59:26 -0300

    add math operations endpoint

---
+++ src/main/java/com/thurdass/springboot_lab/controllers/MathController.java
@@ -0,0 +1,37 @@
+package com.thurdass.springboot_lab.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    @GetMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(
+            @PathVariable String numberOne,
+            @PathVariable String numberTwo) {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new IllegalArgumentException("Please provide valid numeric values.");
+        }
+
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        String number = strNumber.replace(",", ".");
+        return Double.parseDouble(number);
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null || strNumber.isBlank()) {
+            return false;
+        }
+
+        String number = strNumber.replace(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
