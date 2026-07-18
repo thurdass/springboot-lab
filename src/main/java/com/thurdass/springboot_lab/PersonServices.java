6758a77c2bf69ac886870b9b1ecabbdad3df56fe
ThurDas <va4591073@gmail.com>
2026-07-18 13:53:44 -0300

    Feat: add the class PersonServices

--- src/main/java/com/thurdass/springboot_lab/PersonServices.java
+++ src/main/java/com/thurdass/springboot_lab/PersonServices.java
@@ -1,7 +1,28 @@
 package com.thurdass.springboot_lab;
 
+
+import com.thurdass.springboot_lab.model.Person;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public Person findById(String id) {
+        logger.info("Finding one Person!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirtsName("Leandro");
+        person.setLastName("Costa");
+        person.setAdress("Uberlândia - Minas Gerais - Brasil");
+        person.setGender("Male");
+        return person;
+    }
 }
