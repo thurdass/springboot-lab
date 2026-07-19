a6bc5cfca8c4db266fae8764b28c0acdc8a3a422
ThurDas <va4591073@gmail.com>
2026-07-19 20:16:22 -0300

    feat: update class PersonServices

--- src/main/java/com/thurdass/springboot_lab/PersonServices.java
+++ src/main/java/com/thurdass/springboot_lab/PersonServices.java
@@ -4,6 +4,8 @@ package com.thurdass.springboot_lab;
 import com.thurdass.springboot_lab.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,21 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
 
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+
+        logger.info("Finding all People!");
+
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+    }
 
     public Person findById(String id) {
         logger.info("Finding one Person!");
--- src/main/java/com/thurdass/springboot_lab/exception/handler/customEntityResponseHandler.java
+++ src/main/java/com/thurdass/springboot_lab/exception/handler/customEntityResponseHandler.java
@@ -2,7 +2,6 @@ package com.thurdass.springboot_lab.exception.handler;
 
 import com.thurdass.springboot_lab.exception.ExceptionResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
