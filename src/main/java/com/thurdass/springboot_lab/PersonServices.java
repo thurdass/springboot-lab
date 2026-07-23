b61cb673639679ad6e5c8007fb9ef2176dd0bc1a
ThurDas <va4591073@gmail.com>
2026-07-22 22:34:21 -0300

    Feat: postman update

--- src/main/java/com/thurdass/springboot_lab/PersonServices.java
+++ src/main/java/com/thurdass/springboot_lab/PersonServices.java
@@ -17,9 +17,7 @@ public class PersonServices {
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
     public List<Person> findAll() {
-
         logger.info("Finding all People!");
-
         List<Person> persons = new ArrayList<Person>();
         for (int i = 0; i < 8; i++) {
             Person person = mockPerson(i);
@@ -27,7 +25,6 @@ public class PersonServices {
         }
         return persons;
     }
-
 
 
     public Person findById(String id) {
@@ -41,6 +38,7 @@ public class PersonServices {
         person.setGender("Male");
         return person;
     }
+
     private Person mockPerson(int i) {
         Person person = new Person();
         person.setId(counter.incrementAndGet());
@@ -49,5 +47,23 @@ public class PersonServices {
         person.setAdress("Some Address in Brasil");
         person.setGender("Male");
         return person;
+    }
+
+    public Person create(Person person) {
+        logger.info("Creating one person!");
+        return person;
+    }
+
+    public Person update(Person person) {
+
+        logger.info("Updating one Person!");
+
+        return person;
+    }
+
+    public void delete(String id) {
+
+        logger.info("Deleting one Person!");
+
     }
 }
