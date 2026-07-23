64c17e4080fe9cb6368f9f0093062ab675f1d2c2
ThurDas <va4591073@gmail.com>
2026-07-22 22:34:21 -0300

    Feat: postman update

--- src/main/java/com/thurdass/springboot_lab/controllers/PersonController.java
+++ src/main/java/com/thurdass/springboot_lab/controllers/PersonController.java
@@ -30,4 +30,29 @@ public class PersonController {
         return service.findById(id);
     }
 
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person update(@RequestBody Person person) {
+        return service.update(person);
+    }
+
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.DELETE
+    )
+    public void delete(@PathVariable("id") String id) {
+        service.delete(id);
+    }
 }
