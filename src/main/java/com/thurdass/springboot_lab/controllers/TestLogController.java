8fdc336f7462944c8aac7a8ffd21289276672385
ThurDas <va4591073@gmail.com>
2026-08-05 13:52:11 -0300

    feat: add a new function in the TestLogCOntroller

--- src/main/java/com/thurdass/springboot_lab/controllers/TestLogController.java
+++ src/main/java/com/thurdass/springboot_lab/controllers/TestLogController.java
@@ -1,7 +1,18 @@
 package com.thurdass.springboot_lab.controllers;
 
+import com.thurdass.springboot_lab.services.PersonServices;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 public class TestLogController {
+
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+   @GetMapping("/test")
+    public String testLog() {
+        return "Logs generated successfully";
+    }
 }
