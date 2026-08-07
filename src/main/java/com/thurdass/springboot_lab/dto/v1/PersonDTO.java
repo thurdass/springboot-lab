72b9ea82b2d854043c9532154dc0c1dcee2982cd
ThurDas <va4591073@gmail.com>
2026-08-07 00:15:41 -0300

    feat: add version one person DTO

---
+++ src/main/java/com/thurdass/springboot_lab/dto/v1/PersonDTO.java
@@ -0,0 +1,53 @@
+package com.thurdass.springboot_lab.dto.v1;
+
+public class PersonDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String adress;
+    private String gender;
+
+    public PersonDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
