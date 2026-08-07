d944c3bc748220d1a07524fd44eb03d5d62ca3fa
ThurDas <va4591073@gmail.com>
2026-08-07 00:18:24 -0300

    feat: expose versioned person endpoints

--- src/main/java/com/thurdass/springboot_lab/controllers/PersonController.java
+++ src/main/java/com/thurdass/springboot_lab/controllers/PersonController.java
@@ -2,6 +2,8 @@ package com.thurdass.springboot_lab.controllers;
 
 import com.thurdass.springboot_lab.dto.PersonRequest;
 import com.thurdass.springboot_lab.dto.PersonResponse;
+import com.thurdass.springboot_lab.dto.v1.PersonDTO;
+import com.thurdass.springboot_lab.dto.v2.PersonDTOV2;
 import com.thurdass.springboot_lab.services.PersonServices;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -29,6 +31,16 @@ public class PersonController {
     )
     public PersonResponse findById(@PathVariable("id") Long id) {
         return service.findById(id);
+    }
+
+    @GetMapping(value = "/v1/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonDTO findByIdV1(@PathVariable("id") Long id) {
+        return service.findByIdV1(id);
+    }
+
+    @GetMapping(value = "/v2/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonDTOV2 findByIdV2(@PathVariable("id") Long id) {
+        return service.findByIdV2(id);
     }
 
     @PostMapping(
